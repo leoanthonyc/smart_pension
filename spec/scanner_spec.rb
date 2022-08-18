4728c4008f960cb21058baee4cb833b068b593f7
Leo Catane <leoanthony.catane@protonmail.com>
2022-08-18 12:20:39 +0800

    Add Scanner spec

---
+++ spec/scanner_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require_relative '../scanner'
+
+describe PageVisitScanner do
+  let(:webserverlog_io) { StringIO.new(webserverlog_str) }
+  let(:webserverlog_str) {
+    <<~HEREDOC
+    /help_page/1 126.318.035.038
+    /contact 184.123.665.067
+    /home 184.123.665.067
+    HEREDOC
+  }
+
+  it "yields both path and IP address" do
+    scanner = PageVisitScanner.new(webserverlog_io)
+    expect { |b| scanner.each_entry(&b) }
+      .to yield_successive_args(
+        ['/help_page/1', '126.318.035.038'],
+        ['/contact', '184.123.665.067'],
+        ['/home', '184.123.665.067'],
+      )
+  end
+end
