89de21aae63152ba70ae5c430766ac95e80d81dd
Leo Catane <leoanthony.catane@protonmail.com>
2022-08-18 15:59:22 +0800

    Implement PageViewRepository

--- repository.rb
+++ repository.rb
@@ -1,14 +1,35 @@
 class Repository
+  def store
+    raise "Not Implemented"
+  end
+
   private
+
   def repository
-    @repository ||= repository
+    @repository ||= {}
   end
 end
 
-class PageVisitRepository < Repository
-  def store(path, ip_address)
+class PageViewRepository < Repository
+  def store(url_path, ip_address)
+    repository[url_path] ||= {
+      path: url_path,
+      hits: 0,
+      unique_hits: 0,
+      ips: Set.new
+    }
+    repository[url_path][:hits] += 1
+    unless repository[url_path][:ips].include?(ip_address)
+      repository[url_path][:unique_hits] += 1
+      repository[url_path][:ips].add(ip_address)
+    end
   end
 
-  def sort_by_hits(unique_ip_address: false)
+  def sort_by_hits
+    repository.values.sort_by { |x| x[:hits] }.reverse
+  end
+
+  def sort_by_unique_hits
+    repository.values.sort_by { |x| x[:unique_hits] }.reverse
   end
 end
